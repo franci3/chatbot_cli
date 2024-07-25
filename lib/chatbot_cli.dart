0262cb1760aa4eae4668534773227e57683fa676
franci3 <vela_franco@yahoo.de>
2024-07-25 15:49:21 +0200

    initial commit

--- lib/chatbot_cli.dart
+++ lib/chatbot_cli.dart
@@ -1,12 +1,15 @@
+import 'package:cli_util/cli_logging.dart';
 import 'package:google_gemini/google_gemini.dart';
 
 GoogleGemini? gemini;
+final logger = Logger.standard();
 
 void generateResponse(String input, String apiKey) async {
   gemini = GoogleGemini(apiKey: apiKey);
-
-  gemini
+  final progress = logger.progress('Generating your query: $input');
+  await gemini
       ?.generateFromText(input)
-      .then((value) => print(value.text))
-      .catchError((e) => print(e));
+      .then((value) => logger.stdout(logger.ansi.emphasized(value.text)))
+      .catchError((e) => logger.stderr(e));
+  progress.finish(showTiming: true);
 }
